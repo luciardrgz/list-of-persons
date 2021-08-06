3d360e362edb93a77cd8463050636c578bd7effa
lurdrigz <lurdrigz@gmail.com>
2021-08-06 17:18:06 -0300

    Agregadas clase Persona y función mostrarPersonas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("Listado de personas");
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Lucia', 'Rodriguez')
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = '';
+    for (let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
